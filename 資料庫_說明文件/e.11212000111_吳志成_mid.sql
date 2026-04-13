dc24db0bd846e678c477b41672e83475620f650e
chih-chengwu <sc154@gms.tcu.edu.tw>
2026-04-13 09:03:56 +0800

    0413

--- 資料庫_說明文件/e.11212000111_吳志成_mid.sql
+++ 資料庫_說明文件/e.11212000111_吳志成_mid.sql
@@ -5,32 +5,39 @@
 -- 學號: 1101234
 -- 姓名: 王小明
 
---    題目 1
-SELECT ...
+--    題目 1 : 請列出 ProductTbl 的以下欄位 chProdNo, chProdName, chUnit, rlStockQty, chCreatDateTime 
+--             條件: chCreatDateTime 是最近3個月內建立的資料
+
+SELECT chProdNo, chProdName, chUnit, rlStockQty, chCreatDateTime  
+from ProductTbl (nolock)
+where chCreatDateTime >='1150113'
 
 
-GO
 
 --    題目 2
 SELECT ...
 
 
-GO
-
---    題目 3
-SELECT ...
 
 
-GO
+/*    題目 3: 請說明
+					Select * from DB_TEST.dbo.ProductTbl (nolock)
+                    Select * from ProductTbl
+					以上兩個指令差別在那裡
+*/
+/*
+ANS:....................
+    ....................
 
+
+*/
 --    題目 4
 SELECT ...
 
 
-GO
+
 
 --    題目 5
 SELECT ...
 
 
-GO
